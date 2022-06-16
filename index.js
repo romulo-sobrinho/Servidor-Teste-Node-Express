3caa874a300ef232069c13a4d486346c024cadba
Romulo <romulosilva001@gmail.com>
2022-06-16 11:39:42 -0300

    Movendo pasta assests para dentro da pasta client

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
-app.use("/meuservidor", express.static(path.join(__dirname, 'client')));
+app.use(express.static(path.join(__dirname, 'client')));
 
 app.listen(PORT, ()=>{
   console.log(`Servidor rodando com sucesso na porta: ${PORT}`);
