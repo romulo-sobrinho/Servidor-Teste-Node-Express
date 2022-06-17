feb0b2398eb69fa503fb1902cf0425a00761a02e
Romulo <romulosilva001@gmail.com>
2022-06-17 18:51:08 -0300

    Inclusão de métodos GET, POST, PUT e DELETE de forma básica para uso no insomnia

--- index.js
+++ index.js
@@ -3,7 +3,23 @@ const path = require('path');
 const app = express();
 const PORT = 5000;
 
-app.use(express.static(path.join(__dirname, 'client')));
+app.use("/meuservidor", express.static(path.join(__dirname, 'client')));
+
+app.get("/", (req, res)=>{
+  res.send("<h1>Servidor acessado via método GET</h1>");
+})
+
+app.post("/", (req, res)=>{
+  res.send("<h1>Servidor acessado via método POST</h1>");
+})
+
+app.put("/", (req, res)=>{
+  res.send("<h1>Servidor acessado via método PUT</h1>");
+})
+
+app.delete("/", (req, res)=>{
+  res.send("<h1>Servidor acessado via método DELETE</h1>");
+})
 
 app.listen(PORT, ()=>{
   console.log(`Servidor rodando com sucesso na porta: ${PORT}`);
